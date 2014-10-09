1efe060f882c653f3d9b80064d15637ff64b4075
Rich Trott <rtrott@gmail.com>
2014-10-09 14:28:56 -0700

    include link to all results, closes #20

--- index.js
+++ index.js
@@ -2,37 +2,28 @@ var querystring = require('querystring');
 var cheerio = require('cheerio');
 var http = require('http');
 var extend = require('util-extend');
+var url = require('url');
 
 var options = {
-    host: 'ucelinks.cdlib.org',
-    port: 8888,
-    path: '/sfx_ucsf/az'
+    url: 'http://ucelinks.cdlib.org:8888/sfx_ucsf/az'
 };
 
 exports.setOptions = function (newOptions) {
     options = extend(options, newOptions);
-    if (newOptions.host) {
-        options.hostname = newOptions.host;
-    }
 };
 
 exports.search = function (query, callback) {
     'use strict';
-
-    var myOptions = options;
 
     if (! query || ! query.searchTerm) {
         callback(null, {data: []});
         return;
     }
 
-    myOptions.path = myOptions.path + '?param_textSearchType_value=startsWith&' +
+    var myUrl = options.url + '?param_textSearchType_value=startsWith&' +
             querystring.stringify({param_pattern_value: query.searchTerm});
 
-    var port = myOptions.port;
-    var hostname = myOptions.hostname || myOptions.host;
-
-    http.get(myOptions, function (resp) {
+    http.get(myUrl, function (resp) {
         var rawData = '';
 
         resp.on('data', function (chunk) {
@@ -43,26 +34,20 @@ exports.search = function (query, callback) {
             var $ = cheerio.load(rawData);
             var result = [];
             $('a.Results').each(function () {
-                var url = $(this).attr('href');
-                if (typeof url === 'string') {
-                    url = url.trim();
+                var href = $(this).attr('href');
+                if (typeof href === 'string') {
+                    href = href.trim();
                     
-                    if (! url.match(/^(https?:)?\/\/.+/)) {
-                        var fqdn = 'http://' + hostname;
-                        if (port) {
-                            fqdn = fqdn + ':' + port;
-                        }
-                        url = fqdn + url;
-                    }
+                    href = url.resolve(myUrl, href);
 
                     result.push({
                         name: $(this).text(),
-                        url: url
+                        url: href
                     });
                 }
             });
 
-            callback(null, {data: result});
+            callback(null, {data: result, url: myUrl});
         });
     }).on('error', function (e) {
         callback(e);
